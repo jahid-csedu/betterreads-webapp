8657a26fa4a88b42167da8e30ed72e3825cd5702
Md. Jahid Hasan <jahid.csedu@gmail.com>
2021-11-05 12:51:47 +0600

    initialized repository with find book by id

---
+++ src/main/java/com/example/betterreads/book/BookController.java
@@ -0,0 +1,35 @@
+package com.example.betterreads.book;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class BookController {
+	
+	private final String COVER_IMAGE_ROOTS = "http://covers.openlibrary.org/b/id/";
+	
+	@Autowired
+	BookRepository bookRepository;
+	
+	@GetMapping("/books/{bookId}")
+	public String getBook(@PathVariable String bookId, Model model) {
+		Optional<Book> optionalBook =  bookRepository.findById(bookId);
+		if(optionalBook.isPresent()) {
+			Book book = optionalBook.get();
+			String coverImageUrl = "/images/no-image.png";
+			if (book.getCoverIds() != null && book.getCoverIds().size() > 0) {
+				coverImageUrl = COVER_IMAGE_ROOTS + book.getCoverIds().get(0) + "-L.jpg";
+			}
+			model.addAttribute("coverImage", coverImageUrl);
+			model.addAttribute("book", book);
+			return "book";
+		}
+		return "book-not-found";
+	}
+
+}
